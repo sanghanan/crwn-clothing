f60e861c21d5041adc5b1cd0b25b94382dd16da3
Sanjitha Singh <sanjitha.singh@gmail.com>
2020-09-16 16:23:06 +0530

    moved initial shop and category data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,24 @@
 /* eslint-disable require-jsdoc */
-import React, {Component} from 'react';
-import SHOP_DATA from './shop.data.js';
-import PreviewCollection from
-  '../../components/preview-collection/preview-collection.component';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import PreviewCollection from "../../components/preview-collection/preview-collection.component";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state={collections: SHOP_DATA};
-  }
-  render() {
-    const {collections}=this.state;
-    return (
+const ShopPage = ({ collections }) => {
+  return (
+    <div>
       <div className="shop-page">
-        {
-          collections.map(({id, ...otherCollectioProps}) =>
-            (<PreviewCollection key={id} {...otherCollectioProps}/>))
-        }
-
+        {collections.map(({ id, ...otherCollectioProps }) => (
+          <PreviewCollection key={id} {...otherCollectioProps} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
